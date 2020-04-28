f901c5a6eeda23c92216d552f64e0f885ccd5373
Dedy Yuristiawan <dedy@thefthing.com>
2020-04-28 19:05:59 +0700

    Initial Commit

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Dedy Yuristiawan on 28/04/20.
+//  Copyright © 2020 Dedy Yuristiawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
